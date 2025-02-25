53ee5045cca56db837e03e63225c48f40e391b7a
elSolitarioJorge <Liuhou77@outlook.com>
2025-02-25 20:33:43 +0800

    [704]二分查找

---
+++ 704_binary-search.c
@@ -0,0 +1,16 @@
+int search(int* nums, int numsSize, int target) {
+    int left = 0, right = numsSize - 1;
+    while(left <= right){
+        int middle = left + (right - left) / 2;
+        if(nums[middle] < target){
+            left = middle + 1;
+        } else if(nums[middle] > target){
+            right = middle - 1;
+        } else {
+            return middle;
+        }
+    }
+    return -1;
+}
+    
+    
