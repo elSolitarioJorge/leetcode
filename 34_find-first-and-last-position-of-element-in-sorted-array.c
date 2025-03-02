2b9b17470b51a1fa0983dd091e72a5bd9bc67e45
elSolitarioJorge <Liuhou77@outlook.com>
2025-03-02 14:09:29 +0800

    [34]在排序数组中查找元素的第一个和最后一个位置————二分查找的三种区间写法

--- 34_find-first-and-last-position-of-element-in-sorted-array.c
+++ 34_find-first-and-last-position-of-element-in-sorted-array.c
@@ -1,28 +1,53 @@
-int findLeftBorder(int* nums, int numsSize, int target){
-    int left = 0, right = numsSize - 1;
-    while(left <= right){
+int lowerBound1(int* nums, int len, int target){
+    int left = 0, right = len - 1;    // 闭区间[left, right]
+    while(left <= right){   //区间不为空
         int middle = left + (right - left) / 2;
         if(nums[middle] < target){
-            left = middle + 1;
+            left = middle + 1;    // [middle + 1, right]
         } else{
-            right = middle - 1;
+            right = middle - 1;    // left[left, middle - 1]
         }
     }
-    return left;
+    return right + 1;
+}
+
+int lowerBound2(int* nums, int numsSize, int target){
+    int left = 0, right = numsSize;    //左闭右开[left, right)
+    while(left < right){ // 区间不为空
+        int middle = left + (right - left) / 2;
+        if(nums[middle] < target){
+            left = middle + 1;    // [middle + 1, right)
+        } else{
+            right = middle;    // [left, middle)
+        }
+    }
+    return right;
+}
+
+int lowerBound3(int* nums, int numsSize, int target){
+    int left = -1, right = numsSize;    //开区间(left, right)
+    while(left + 1 != right){ // 区间不为空
+        int middle = left + (right - left) / 2;
+        if(nums[middle] < target){
+            left = middle;    // (middle, right)
+        } else{
+            right = middle;    // (left, middle)
+        }
+    }
+    return right;
 }
 
 int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
     int* ans = (int*)malloc(sizeof(int) * 2);
     *returnSize = 2;
-    int start = findLeftBorder(nums, numsSize, target);
+    ans[0] = -1;
+    ans[1] = -1;
+    int start = lowerBound1(nums, numsSize, target);
     if(start == numsSize || nums[start] != target){
-        ans[0] = -1;
-        ans[1] = -1;
         return ans;
     }
-    int end = findLeftBorder(nums, numsSize, target + 1) - 1;
     ans[0] = start;
-    ans[1] = end;
+    ans[1] = lowerBound1(nums, numsSize, target + 1) - 1;
     return ans;
 }
     
