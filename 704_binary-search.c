ddc8ea4d7e6b6fe3f1764800a6cfbecc3a9d3019
elSolitarioJorge <Liuhou77@outlook.com>
2025-03-02 14:19:58 +0800

    [704]二分查找————二分查找（开区间）

--- 704_binary-search.c
+++ 704_binary-search.c
@@ -1,16 +1,14 @@
 int search(int* nums, int numsSize, int target) {
-    int left = 0, right = numsSize - 1;
-    while(left <= right){
+    int left = -1, right = numsSize;
+    while(left + 1 != right){
         int middle = left + (right - left) / 2;
         if(nums[middle] < target){
-            left = middle + 1;
-        } else if(nums[middle] > target){
-            right = middle - 1;
-        } else {
-            return middle;
+            left = middle;
+        } else{
+            right = middle;
         }
     }
-    return -1;
+    return (right == numsSize || nums[right] != target) ? -1 : right;
 }
     
     
