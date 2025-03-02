1d8e681541ccf3e1c5d6ee13c54834a3045fbcf0
elSolitarioJorge <Liuhou77@outlook.com>
2025-03-02 14:15:04 +0800

    [35]搜索插入位置————二分查找（开区间）

--- 35_search-insert-position.c
+++ 35_search-insert-position.c
@@ -1,16 +1,14 @@
 int searchInsert(int* nums, int numsSize, int target) {
-    int left = 0, right = numsSize - 1;
-    while(left <= right){
+    int left = -1, right = numsSize;
+    while(left + 1 != right){
         int middle = left + (right - left) / 2;
         if(nums[middle] < target){
-            left = middle + 1;
-        } else if(nums[middle] > target){
-            right = middle - 1;
+            left = middle;
         } else{
-            return middle;
+            right = middle;
         }
     }
-    return left;
+    return right;
 }
     
     
