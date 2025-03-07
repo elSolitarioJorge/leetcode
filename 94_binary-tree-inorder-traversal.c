5b751e05e75d338a12e6e694733f67bbcb351f60
elSolitarioJorge <Liuhou77@outlook.com>
2025-03-07 19:45:24 +0800

    [94]二叉树的中序遍历————迭代（栈）

--- 94_binary-tree-inorder-traversal.c
+++ 94_binary-tree-inorder-traversal.c
@@ -1,17 +1,22 @@
-void inorder(struct TreeNode* root, int* ans, int* returnSize){
-     if(root == NULL){
-         return;
-     }
-     inorder(root->left, ans, returnSize);
-     ans[(*returnSize)++] = root->val;
-     inorder(root->right, ans, returnSize);
- }
-
 int* inorderTraversal(struct TreeNode* root, int* returnSize) {
-     int* ans = (int*)malloc(sizeof(int) * 100);
-     *returnSize = 0;
-     inorder(root, ans, returnSize);
-     return ans;
- }
+    int* ans = (int*)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    if(root == NULL){
+        return ans;
+    }
+    struct TreeNode* stack[100];
+    struct TreeNode* node = root;
+    int top = -1;
+    while(top >= 0 || node != NULL){
+        while(node != NULL){
+            stack[++top] = node;
+            node = node->left;
+        }
+        node = stack[top--];
+        ans[(*returnSize)++] = node->val;
+        node = node->right;
+    }
+    return ans;
+}
     
     
