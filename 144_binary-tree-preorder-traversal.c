a50262f92ef70ca2c0ec878f981b76e882dc8851
elSolitarioJorge <Liuhou77@outlook.com>
2025-03-07 19:24:45 +0800

    [144]二叉树的前序遍历————迭代（栈）

--- 144_binary-tree-preorder-traversal.c
+++ 144_binary-tree-preorder-traversal.c
@@ -1,16 +1,21 @@
-void preorder(struct TreeNode* root, int* ans, int* returnSize){
-    if(root == NULL){
-        return;
-    }
-    ans[(*returnSize)++] = root->val;
-    preorder(root->left, ans, returnSize);
-    preorder(root->right, ans, returnSize);
-}
-
 int* preorderTraversal(struct TreeNode* root, int* returnSize) {
     int* ans = (int*)malloc(sizeof(int) * 100);
     *returnSize = 0;
-    preorder(root, ans, returnSize);
+    if(root == NULL){
+        return ans;
+    }
+    struct TreeNode* stack[100];
+    struct TreeNode* node = root;
+    int top = -1;
+    while(top >= 0 || node != NULL){
+        while(node != NULL){
+            ans[(*returnSize)++] = node->val;
+            stack[++top] = node;
+            node = node->left;
+        }
+        node = stack[top--];
+        node = node->right;
+    }
     return ans;
 }
     
