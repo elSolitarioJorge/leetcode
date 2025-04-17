b698bd66d48239c8b87c101324e323a479028a5c
elSolitarioJorge <Liuhou77@outlook.com>
2025-04-17 22:42:21 +0800

    [62]不同路径

--- [62]不同路径/62_unique-paths.c
+++ [62]不同路径/62_unique-paths.c
@@ -1,17 +1,16 @@
 int uniquePaths(int m, int n) {
-    int dp[m][n];
-    for(int i = 0; i < m; i++){
-        dp[i][0] = 1;
+    if (m == 1 || n == 1) {
+        return 1;
     }
-    for(int j = 0; j < n; j++){
-        dp[0][j] = 1;
-    }
-    for(int i = 1; i < m; i++){
-        for(int j = 1; j < n; j++){
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    int dp[n + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[1] = 1;
+    for (int i = 1; i <= m; i++) {
+        for (int j = 2; j <= n; j++) {
+            dp[j] += dp[j - 1];
         }
     }
-    return dp[m - 1][n - 1];
+    return dp[n];
 }
     
     
