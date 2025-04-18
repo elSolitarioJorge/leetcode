2b5528be830740477e1f9118464f8f39ac92e1b3
elSolitarioJorge <Liuhou77@outlook.com>
2025-04-18 22:57:25 +0800

    [144]二叉树的前序遍历

--- [144]二叉树的前序遍历/144_binary-tree-preorder-traversal.c
+++ [144]二叉树的前序遍历/144_binary-tree-preorder-traversal.c
@@ -1,3 +1,19 @@
+void preorder(struct TreeNode* root, int* ans, int* returnSize){
+    if(root == NULL){
+        return;
+    }
+    ans[(*returnSize)++] = root->val;
+    preorder(root->left, ans, returnSize);
+    preorder(root->right, ans, returnSize);
+}
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* ans = (int*)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    preorder(root, ans, returnSize);
+    return ans;
+}
+
 int* preorderTraversal(struct TreeNode* root, int* returnSize) {
     int* ans = (int*)malloc(sizeof(int) * 100);
     *returnSize = 0;
@@ -15,6 +31,34 @@ int* preorderTraversal(struct TreeNode* root, int* returnSize) {
         }
         node = stack[top--];
         node = node->right;
+    }
+    return ans;
+}
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* ans = (int*)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    if (root == NULL) {
+        return ans;
+    }
+    struct TreeNode* stack[100];
+    int top = -1;
+    stack[++top] = root;
+    while (top >= 0) {
+        struct TreeNode* node = stack[top--];
+        if (node) {
+            if (node->right) {
+                stack[++top] = node->right;
+            }
+            if (node->left) {
+                stack[++top] = node->left;
+            }
+            stack[++top] = node;
+            stack[++top] = NULL;
+        } else {
+            node = stack[top--];
+            ans[(*returnSize)++] = node->val;
+        }
     }
     return ans;
 }
