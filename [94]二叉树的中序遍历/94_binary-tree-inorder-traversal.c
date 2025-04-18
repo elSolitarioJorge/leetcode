a6837e9bd64c09c260258ec421ce8369788a3437
elSolitarioJorge <Liuhou77@outlook.com>
2025-04-18 22:58:31 +0800

    [94]二叉树的中序遍历

--- [94]二叉树的中序遍历/94_binary-tree-inorder-traversal.c
+++ [94]二叉树的中序遍历/94_binary-tree-inorder-traversal.c
@@ -1,3 +1,19 @@
+void inorder(struct TreeNode* root, int* ans, int* returnSize){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left, ans, returnSize);
+    ans[(*returnSize)++] = root->val;
+    inorder(root->right, ans, returnSize);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* ans = (int*)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    inorder(root, ans, returnSize);
+    return ans;
+}
+
 int* inorderTraversal(struct TreeNode* root, int* returnSize) {
     int* ans = (int*)malloc(sizeof(int) * 100);
     *returnSize = 0;
@@ -15,6 +31,34 @@ int* inorderTraversal(struct TreeNode* root, int* returnSize) {
         node = stack[top--];
         ans[(*returnSize)++] = node->val;
         node = node->right;
+    }
+    return ans;
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* ans = (int*)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    if (root == NULL) {
+        return ans;
+    }
+    struct TreeNode* stack[100];
+    int top = -1;
+    stack[++top] = root;
+    while (top >= 0) {
+        struct TreeNode* node = stack[top--];
+        if (node) {
+            if (node->right) {
+                stack[++top] = node->right;
+            }
+            stack[++top] = node;
+            stack[++top] = NULL;
+            if (node->left) {
+                stack[++top] = node->left;
+            }
+        } else {
+            node = stack[top--];
+            ans[(*returnSize)++] = node->val;
+        }
     }
     return ans;
 }
