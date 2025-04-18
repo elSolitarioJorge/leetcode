c84ca51cb5fa9732314ab3de92f78eaec0888a55
elSolitarioJorge <Liuhou77@outlook.com>
2025-04-18 22:59:24 +0800

    [145]二叉树的后序遍历

--- [145]二叉树的后序遍历/145_binary-tree-postorder-traversal.c
+++ [145]二叉树的后序遍历/145_binary-tree-postorder-traversal.c
@@ -1,3 +1,18 @@
+void postorder(struct TreeNode* root, int* ans, int* returnSize){
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left, ans, returnSize);
+    postorder(root->right, ans, returnSize);
+    ans[(*returnSize)++] = root->val;
+}
+int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* ans = (int*)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    postorder(root, ans, returnSize);
+    return ans;
+}
+
 int* postorderTraversal(struct TreeNode* root, int* returnSize) {
     int* ans = (int*)malloc(sizeof(int) * 100);
     *returnSize = 0;
@@ -21,6 +36,34 @@ int* postorderTraversal(struct TreeNode* root, int* returnSize) {
         } else{   //右子结点还有未被访问的结点
             stack[++top] = node;
             node = node->right;
+        }
+    }
+    return ans;
+}
+
+int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* ans = (int*)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    if (root == NULL) {
+        return ans;
+    }
+    struct TreeNode* stack[100];
+    int top = -1;
+    stack[++top] = root;
+    while (top >= 0) {
+        struct TreeNode* node = stack[top--];
+        if (node) {
+            stack[++top] = node;
+            stack[++top] = NULL;
+            if (node->right) {
+                stack[++top] = node->right;
+            }
+            if (node->left) {
+                stack[++top] = node->left;
+            }
+        } else {
+            node = stack[top--];
+            ans[(*returnSize)++] = node->val;
         }
     }
     return ans;
