8fa0cdfec4526183132d7b849d70c87d1b6e12cd
elSolitarioJorge <Liuhou77@outlook.com>
2025-05-11 22:07:37 +0800

    [98]验证二叉搜索树

---
+++ [98]验证二叉搜索树/98_validate-binary-search-tree.java
@@ -0,0 +1,54 @@
+class Solution {
+    private long pre = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        return isValidBST(root.right);
+    }
+}
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode node, long left, long right) {
+        if (node == null) {
+            return true;
+        }
+        long x = node.val;
+        return left < x && x < right && isValidBST(node.left, left, x) && isValidBST(node.right, x, right);
+    }
+}
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode pop = stack.pop();
+            if (pre != null && pop.val <= pre.val) {
+                return false;
+            }
+            pre = pop;
+            root = pop.right;
+        }
+        return true;
+    }
+}
