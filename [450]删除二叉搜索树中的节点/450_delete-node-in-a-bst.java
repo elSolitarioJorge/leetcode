02b3cf4803cd64c5f107f76bcdcde8f9165b4b67
elSolitarioJorge <Liuhou77@outlook.com>
2025-05-17 22:03:34 +0800

    [450]删除二叉搜索树中的节点

---
+++ [450]删除二叉搜索树中的节点/450_delete-node-in-a-bst.java
@@ -0,0 +1,27 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode cur = root.right;
+                while (cur.left != null) {
+                    cur = cur.left;
+                }
+                cur.left = root.left;
+                return root.right;
+            }
+        }
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+}
+    
+    
