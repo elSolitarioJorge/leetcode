e10106c99b0219bbe160a116d0ab97e323a544bd
elSolitarioJorge <Liuhou77@outlook.com>
2025-05-18 22:08:34 +0800

    [538]把二叉搜索树转换为累加树

---
+++ [538]把二叉搜索树转换为累加树/538_convert-bst-to-greater-tree.java
@@ -0,0 +1,20 @@
+class Solution {
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    private void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.right);
+        sum += node.val;
+        node.val = sum;
+        dfs(node.left);
+    }
+}
+    
+    
