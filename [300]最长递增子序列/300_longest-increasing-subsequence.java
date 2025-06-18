e63a19b05cc2b09beb2277dd3eee8064f535228c
elSolitarioJorge <Liuhou77@outlook.com>
2025-06-18 18:09:52 +0800

    [300]最长递增子序列

---
+++ [300]最长递增子序列/300_longest-increasing-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length, ans = 0;
+        int[] f = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    f[i] = Math.max(f[i], f[j]);
+                }
+            }
+            ans = Math.max(ans, ++f[i]);
+        }
+        return ans;
+    }
+}
+    
+    
