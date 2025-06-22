08b5568440c80528a192bd17ad9e84e7c77bdc5c
elSolitarioJorge <Liuhou77@outlook.com>
2025-06-22 17:25:08 +0800

    [404]左子叶之和

---
+++ [404]左子叶之和/404_sum-of-left-leaves.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int res = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left != null && node.left.left == null && node.left.right == null) {
+                res += node.left.val;
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return res;
+    }
+}
+    
+    
