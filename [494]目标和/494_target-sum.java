a82aa818e9e3617cea5ae3ea567646bb4b484bbc
elSolitarioJorge <Liuhou77@outlook.com>
2025-06-28 15:21:43 +0800

    [494]目标和

---
+++ [494]目标和/494_target-sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    private int[] nums;
+    private int[][] memo;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int s = 0;
+        for (int x : nums) {
+            s += x;
+        }
+        s -= Math.abs(target);
+        if (s < 0 || s % 2 == 1) {
+            return 0;
+        }
+        int m = s / 2;
+        this.nums = nums;
+        int n = nums.length;
+        memo = new int[n][m + 1];
+        for (int[] me : memo) {
+            Arrays.fill(me, -1);
+        }
+        return dfs(n - 1, m);
+    }
+
+    private int dfs(int i, int c) {
+        if (i < 0) {
+            return c == 0 ? 1 : 0;
+        }
+
+        if (memo[i][c] != -1) {
+            return memo[i][c];
+        }
+        if (c < nums[i]) {
+            return memo[i][c] = dfs(i - 1, c);
+        }
+        return memo[i][c] = dfs(i - 1, c) + dfs(i - 1, c - nums[i]);
+    }
+}
+    
+    
