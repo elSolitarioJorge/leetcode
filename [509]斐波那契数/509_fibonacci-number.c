a460cf8ff339dc50f9033bf83e95d971c7c0dbc7
elSolitarioJorge <Liuhou77@outlook.com>
2025-06-29 19:34:46 +0800

    [509]斐波那契数

---
+++ [509]斐波那契数/509_fibonacci-number.c
@@ -0,0 +1,15 @@
+int fib(int n) {
+    if (n < 2) {
+        return n;
+    }
+    int dp[2];
+    dp[0] = 0, dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        int sum = dp[0] + dp[1];
+        dp[0] = dp[1];
+        dp[1] = sum;
+    }
+    return dp[1];
+}
+    
+    
