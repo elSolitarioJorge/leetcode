f926d3432652c08503eec492bfed0e64442327b6
elSolitarioJorge <Liuhou77@outlook.com>
2025-06-29 19:40:31 +0800

    [513]找树左下角的值

---
+++ [513]找树左下角的值/513_find-bottom-left-tree-value.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        int res = 0;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                if (i == 0) {
+                    res = node.val;
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+}
+    
+    
