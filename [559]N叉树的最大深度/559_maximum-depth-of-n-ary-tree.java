a2ac58653c02d262fb2b719b1fef2799e725b5f7
elSolitarioJorge <Liuhou77@outlook.com>
2025-07-03 21:49:13 +0800

    [559]N叉树的最大深度

---
+++ [559]N叉树的最大深度/559_maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int maxChildDepth = 0;
+        for (Node child : root.children) {
+            maxChildDepth = Math.max(maxChildDepth, maxDepth(child));
+        }
+        return maxChildDepth + 1;
+    }
+}
+    
+    
