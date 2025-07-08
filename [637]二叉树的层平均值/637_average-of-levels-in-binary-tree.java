8158ffd900210a4e30930c8e042117a757e5b550
elSolitarioJorge <Liuhou77@outlook.com>
2025-07-08 19:34:46 +0800

    [637]二叉树的层平均值

---
+++ [637]二叉树的层平均值/637_average-of-levels-in-binary-tree.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            double sum = 0;
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(sum / n);
+        }
+        return res;
+    }
+}
+    
+    
