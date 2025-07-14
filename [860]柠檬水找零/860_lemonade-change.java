27683a06fb9574c7f06fc05463f79661c6da1019
elSolitarioJorge <Liuhou77@outlook.com>
2025-07-14 20:39:37 +0800

    [860]柠檬水找零

---
+++ [860]柠檬水找零/860_lemonade-change.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int b : bills) {
+            if (b == 5) {
+                five++;
+            } else if (b == 10) {
+                ten++;
+                five--;
+            } else if (ten > 0) {
+                ten--;
+                five--;
+            } else {
+                five -= 3;
+            }
+            if (five < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+    
+    
