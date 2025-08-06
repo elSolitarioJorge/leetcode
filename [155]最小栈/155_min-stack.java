de852666d72e05725a366f8463724e6d0c0bbb3f
elSolitarioJorge <Liuhou77@outlook.com>
2025-08-06 14:15:44 +0800

    [155]最小栈

---
+++ [155]最小栈/155_min-stack.java
@@ -0,0 +1,26 @@
+class MinStack {
+
+    private final Deque<int[]> stack = new ArrayDeque<>();
+
+    public MinStack() {
+        stack.push(new int[]{0, Integer.MAX_VALUE});
+    }
+
+    public void push(int val) {
+        stack.push(new int[]{val, Math.min(getMin(), val)});
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+    
+    
